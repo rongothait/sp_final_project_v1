eba21046a2782cba7c677e9cb267a495fd2a3f7f
rongothait <ron.gothait@gmail.com>
2025-07-08 13:54:10 +0000

    sterted symnf.py

---
+++ symnf.py
@@ -0,0 +1,18 @@
+import numpy as np
+import sys
+
+def main():
+    # 1. read arguments from CMD
+    k = int(sys.argv[1])  # int, number of clusters
+    goal = sys.argv[2]  # 'symnf' / 'sym' / 'ddg' / 'norm'
+    file_name = sys.argv[3]  # path to data set
+
+    if goal == "symnf":
+        pass #TODO
+    elif goal == "sym":
+        pass #TODO
+    elif goal == "ddg":
+        pass #TODO
+    elif goal == "norm":
+        pass #TODO
+
